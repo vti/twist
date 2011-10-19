2fd5634b69973104562678be4f332e90d9aa8012
vti <viacheslav.t@gmail.com>
2011-10-19 20:34:13 +0300

    Cache directory listing

--- lib/Twist/Directory.pm
+++ lib/Twist/Directory.pm
@@ -5,12 +5,31 @@ use warnings;
 
 use base 'Twist::Base';
 
+use Try::Tiny;
 use File::Spec;
+use Storable;
+use Time::HiRes qw(stat);
 
 use Twist::File;
 
 sub files {
     my $self = shift;
+
+    if (my $files = $self->_load_cache) {
+        return $files;
+    }
+
+    my $files = $self->_files;
+
+    $self->_save_cache;
+
+    return $files;
+}
+
+sub _files {
+    my $self = shift;
+
+    return @{$self->{files}} if $self->{files};
 
     my @files;
     opendir my $dh, $self->{path} or die "Can't open `$self->{path}`: $!";
@@ -20,11 +39,61 @@ sub files {
         $file = File::Spec->catfile($self->{path}, $file);
         next unless -f $file;
 
-        push @files, Twist::File->new(path => $file);
+        push @files, $file;
     }
     close $dh;
 
-    return [sort { $b->created->epoch <=> $a->created->epoch } @files];
+    $self->{files} = [
+        sort { $b->created->epoch <=> $a->created->epoch }
+        map { Twist::File->new(path => $_) } @files
+    ];
+
+    return $self->{files};
+}
+
+sub _path_to_cache {
+    my $self = shift;
+
+    return File::Spec->catfile($self->{path}, '.cache');
+}
+
+sub _load_cache {
+    my $self = shift;
+
+    return unless -f $self->_path_to_cache;
+
+    open my $fh, '<', $self->_path_to_cache or return;
+
+    return try {
+        my $cache = Storable::retrieve_fd($fh);
+
+        my @stat = stat($self->{path});
+        if ($cache->{mtime} ne $stat[9]) {
+            return;
+        }
+
+        return $cache->{files};
+    }
+    catch {
+        unlink $self->_path_to_cache;
+
+        return;
+    };
+}
+
+sub _save_cache {
+    my $self = shift;
+
+    my $files = $self->{files};
+
+    open my $fh, '>', $self->_path_to_cache or return;
+
+    try {
+        my @stat = stat($self->{path});
+        Storable::nstore_fd({files => $files, mtime => $stat[9]}, $fh);
+    };
+
+    return;
 }
 
 1;
--- t/directory.t
+++ t/directory.t
@@ -3,12 +3,55 @@ use warnings;
 
 use Test::Spec;
 
+use File::stat;
+use Time::HiRes qw(usleep);
+
 use_ok('Twist::Directory');
 
 describe 'directory' => sub {
     it 'should load files' => sub {
+        unlink 't/directory/.cache';
+
         my $dir = Twist::Directory->new(path => 't/directory');
         is($dir->files->[0]->filename, 'foo');
+    };
+
+    it 'should cache files' => sub {
+        unlink 't/directory/.cache';
+
+        my $dir = Twist::Directory->new(path => 't/directory');
+        $dir->files;
+        undef $dir;
+
+        ok(-f 't/directory/.cache');
+
+        unlink 't/directory/.cache';
+    };
+
+    it 'should reread directory when it changes' => sub {
+        unlink 't/directory/.cache';
+
+        my $dir = Twist::Directory->new(path => 't/directory');
+        $dir->files;
+        undef $dir;
+
+        my $old_stat = stat 't/directory/.cache';
+
+        usleep 10000;
+
+        open my $fh, '>', 't/directory/new_file';
+        print $fh 'foo';
+        close $fh;
+
+        $dir = Twist::Directory->new(path => 't/directory');
+        $dir->files;
+        undef $dir;
+
+        my $new_stat = stat 't/directory/.cache';
+
+        isnt($old_stat->size, $new_stat->size);
+
+        unlink 't/directory/new_file';
     };
 };
 
